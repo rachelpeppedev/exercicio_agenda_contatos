8bdca47a0cc08e9eb05ae17f6df2ce3b1d5851ae
Rachel Peppe <rapeppe@hotmail.com>
2023-10-03 14:28:20 -0300

    Commit inicial

---
+++ main.js
@@ -0,0 +1,27 @@
+const form = document.getElementById('form-agenda');
+const imgRegistrado = '<img src="./imagens/registrado.png" alt="Registrado agora" />';
+let linhas = '';
+
+form.addEventListener('submit', function (e){
+    e.preventDefault();
+
+    const inputNomeContato = document.getElementById('nome-contato');
+    const inputSobrenomeContato = document.getElementById('sobrenome-contato');
+    const inputTelefoneContato = document.getElementById('telefone-contato');
+    
+    let linha = '<tr>';
+    linha += `<td>${inputNomeContato.value}</td>`;
+    linha += `<td>${inputSobrenomeContato.value}</td>`;
+    linha += `<td>${inputTelefoneContato.value}</td>`;
+    linha += `<td>${imgRegistrado}</td>`;
+    linha += '</tr>';
+
+    linhas += linha;
+
+    const corpoTabela = document.querySelector('tbody');
+    corpoTabela.innerHTML = linhas;
+
+    inputNomeContato.value = '';
+    inputSobrenomeContato.value = '';
+    inputTelefoneContato.value = '';
+});
